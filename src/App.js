f7674e245123141fc4441ae37b8ea42ac2e1cf83
Mohd Arshad <mohdarshad8668@gmail.com>
2023-02-25 02:52:38 +0530

    added createStudent

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SignUp from './Pages/signup';
 import Login from './Pages/login';
+import AddStudent from './Pages/addStudent';
 
 
 function App() {
@@ -13,14 +14,15 @@ function App() {
       <BrowserRouter>
         <div className='logo'><h2>Student Management</h2></div>
         <div className='route'>
-        <Routes>
+          <Routes>
             {/* <Route path='/' element={<Home />} /> */}
-            
+
             <Route path='/' element={<Login />} />
             <Route path='signup' element={<SignUp />} />
+            <Route path='addStudent' element={<AddStudent />} />
 
 
-           
+
           </Routes>
         </div>
       </BrowserRouter>
