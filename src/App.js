427e62bd2b9c6d1845142cb59342653534762de4
Mohd Arshad <mohdarshad8668@gmail.com>
2023-02-24 22:48:50 +0530

    firstCommit npm i, create-react-app

---
+++ src/App.js
@@ -0,0 +1,23 @@
+// import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+
+
+function App() {
+  return (
+
+    <div className="App">
+
+      <BrowserRouter>
+        <div className='logo'><h2>Student Management</h2></div>
+        <div className='route'>
+          
+        </div>
+      </BrowserRouter>
+
+    </div>
+  );
+}
+
+export default App;
