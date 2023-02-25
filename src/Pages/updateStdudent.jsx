99fff8b19125a64250ffeaecb1bd6ad2582500b6
Mohd Arshad <mohdarshad8668@gmail.com>
2023-02-25 20:49:01 +0530

    added some styling to login and signup screen

--- src/Pages/updateStdudent.jsx
+++ src/Pages/updateStdudent.jsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate, useParams } from "react-router-dom"
-import React, { useState, useEffect } from "react"
+import React, { useState} from "react"
 
 
 const UpdateStudent = () => {
