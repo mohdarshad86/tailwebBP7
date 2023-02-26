04c1ecee51c076b120e58e9d9282ef9fb54fd649
Mohd Arshad <mohdarshad8668@gmail.com>
2023-02-26 18:50:16 +0530

    added components for popup form, header and logout

---
+++ src/components/logout.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function LogOut() {
+
+    const navigate = useNavigate()
+
+    return <button className="logout" onClick={async (e) => {
+
+        e.preventDefault();
+        localStorage.removeItem("token")
+        let result = await fetch('http://localhost:3001/logout', {
+            method: "post",
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        result = await result.json()
+
+        if (result.status === false) {
+            alert(result.message)
+        } else {
+            console.log(result.data, "**");
+            // setStudentData(result.data)
+            navigate('/')
+        }
+
+    }}>LogOut</button>;
+}
